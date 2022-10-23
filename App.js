a01f76c4cfe672e3fc44863d78e2973f225ebde9
yuryi.ustinovich <ustinhtc@gmail.com>
2022-10-23 13:50:01 +0300

    remove redundant mess (old hardcoded data), add loading indicator

--- App.js
+++ App.js
@@ -1,77 +1,43 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Alert } from 'react-native';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Alert, ActivityIndicator } from 'react-native';
 import SpendGridCell from './components/spends/SpendGridCell'
 import { groupDataByDate } from './helpers/data/dataModifiers'
 
-var today = new Date();
-var oldDate = new Date(new Date().setDate(today.getDate()-5));
-
-const SPENDS = [
-  { 
-    id: 1,
-    comment: "groceries",
-    amount: 15.567,
-    userId: 1,
-    typeId: 0,
-    subType: 1,
-    direction: 0,
-    date: today
-  },
-  { 
-    id: 2,
-    comment: "transport",
-    amount: 3.40,
-    userId: 1,
-    typeId: 2,
-    subType: 5,
-    direction: 0,
-    date: today
-  },
-  { 
-    id: 3,
-    comment: "bouth glasses",
-    amount: 967.45,
-    userId: 1,
-    typeId: 3,
-    subType: 7,
-    direction: 0,
-    date: oldDate
-  },
-  { 
-    id: 4,
-    comment: "salary",
-    amount: 3400,
-    userId: 1,
-    typeId: 12,
-    subType: 15,
-    direction: 1,
-    date: oldDate
-  },
-];
-
-const groupedSpends = groupDataByDate(SPENDS);
-
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [spends, setSpends] = useState([]);
 
   useEffect(() => {
+    setIsLoading(true);
     axios.get('https://63552286483f5d2df3adc834.mockapi.io/get/spends')
     .then(({ data }) => {
-      console.log('raw fetched:', data);
+      //console.log('raw fetched:', data);
       const groupedData = groupDataByDate(data);
-      console.log('grouped fetched:', groupedData);
+      //console.log('grouped fetched:', groupedData);
       setSpends(groupedData);
     }).catch((err) => {
       console.log('error:', err);
-      console.log('error:', err.response);
       Alert.alert('Error','Error during fetching spends form server.');
+    }).finally(() => {
+      setIsLoading(false);
     });
   }, []);
 
   const renderItem = ({ item }) => (
     <SpendGridCell spent={item} />
   );
+
+  if (isLoading) {
+    return <View style={{
+      flex: 1, //takes all the width
+      justifyContent: 'center',
+      alignContent: 'center'
+    }}>
+      <ActivityIndicator size="large" />
+      <Text style={{ marginTop: 15 }} >Data loading..</Text>
+    </View>
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -90,6 +56,7 @@ const App = () => {
               ))
           }
       </View>
+      <StatusBar theme="auto" />
     </SafeAreaView>
   );
 }
